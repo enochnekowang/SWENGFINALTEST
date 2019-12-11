a6f2f3c4fe871304121c3dce28e26f85c48705a9
enochnekowang <58448136+enochnekowang@users.noreply.github.com>
2019-12-11 18:07:50 -0500

    Jacob is a Genie

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.lang.String;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Bryant is awesome");
+        System.out.println("Test Begin");
         Pokedex MyPokedex = new Pokedex();
         int userselection;
 
@@ -14,5 +14,6 @@ public class Main {
         userselection = input.nextInt();
         MyPokedex.GetPokemon(userselection);
 
+        System.out.println("End Test");
     }
 }
